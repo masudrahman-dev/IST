5d30b8aebcf9d6f28e9f6ccaa9d1495280bf28ea
masudrahman-dev <masudrahman.dev@gmail>
2023-04-09 08:10:49 +0600

    armstrong number

---
+++ IST/icu_long_contest_2/other/armstrong-number.c
@@ -0,0 +1,120 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int num, originalNum, remainder, result = 0;
+//     printf("Enter a three digit integer: \n");
+//     scanf("%d", &num);
+//     originalNum = num;
+//     while (originalNum != num)
+//     {
+//         // remainder constains the last digit
+//         remainder = originalNum % 10;
+//         result += remainder * remainder * remainder;
+//         // removing last digit from the original number
+//         originalNum /= 10;
+//     }
+
+//     if (result == num)
+//     {
+//         printf("%d is an Armstrong number.\n",num);
+
+//     }else{
+//         printf("%d is not an Armstrong number.\n",num);
+
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int num, originalNum, remainder, result = 0;
+//     printf("Enter a three-digit integer: ");
+//     scanf("%d", &num);
+//     originalNum = num;
+
+//     while (originalNum != 0)
+//     {
+//         // remainder contains the last digit
+//         remainder = originalNum % 10;
+
+//         result += remainder * remainder * remainder;
+
+//         // removing last digit from the orignal number
+//         originalNum /= 10;
+//     }
+
+//     if (result == num)
+//         printf("%d is an Armstrong number.", num);
+//     else
+//         printf("%d is not an Armstrong number.", num);
+
+//     return 0;
+// }
+// #include <math.h>
+// #include <stdio.h>
+// int main()
+// {
+//     int num, originalNum, remainder, n = 0;
+//     float result = 0.0;
+//     printf("Enter an integer:\n");
+//     scanf("%d", &num);
+//     originalNum = num;
+//     // store the number if digits of num in n
+//     for (originalNum = num; originalNum != 0; ++n)
+//     {
+//         originalNum /= 10;
+//     }
+//     for (originalNum = num; originalNum != 0; originalNum /= 10)
+//     {
+//         remainder = originalNum % 10;
+//         // store the sum of the power of individual digits in result
+//         result = +pow(remainder, n);
+//     }
+//     // if num is wqual to result, the number is an Armstrong number
+//     if ((int)result == num)
+//     {
+//         printf("%d is an Armstrong number.", num);
+//     }
+//     else
+//     {
+//         printf("%d is not an Armstrong number.", num);
+//     }
+
+//     return 0;
+// }
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    int num, originalNum, remainder, n = 0;
+    float result = 0.0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    // store the number of digits of num in n
+    for (originalNum = num; originalNum != 0; ++n)
+    {
+        originalNum /= 10;
+    }
+
+    for (originalNum = num; originalNum != 0; originalNum /= 10)
+    {
+        remainder = originalNum % 10;
+
+        // store the sum of the power of individual digits in result
+        result += pow(remainder, n);
+    }
+
+    // if num is equal to result, the number is an Armstrong number
+    if ((int)result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+    return 0;
+}
